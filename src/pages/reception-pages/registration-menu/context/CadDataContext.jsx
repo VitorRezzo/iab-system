6eebdd9e45af6583d77043e0daaaa9062f782dff
Vitor Rezzo <cv.rezz99@gmail.com>
2023-05-30 22:41:05 -0300

    remove: delete unused variables

--- src/pages/reception-pages/registration-menu/context/CadDataContext.jsx
+++ src/pages/reception-pages/registration-menu/context/CadDataContext.jsx
@@ -7,15 +7,9 @@ export function useCadDataContext() {
 }
 
 export function CadDataContextProvider({ children }) {
-  const [datacad, setDataCad] = useState([]);
   const [dataMove, setDataMove] = useState([]);
 
-  const [quantform, setQuantForm] = useState(0);
   const value = {
-    quantform,
-    setQuantForm,
-    datacad,
-    setDataCad,
     setDataMove,
     dataMove
   };
